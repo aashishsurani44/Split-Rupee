22d7017506e831a1ff5a8bfe8caf425bfc1a9881
aashishsurani44 <aashishsurani44@gmail.com>
2026-08-11 12:26:47 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyBBvRNKV3e9ZPgBoduT2HNr2xnAEY4dlbg",
-  authDomain: "split-rupee.firebaseapp.com",
-  databaseURL: "https://split-rupee-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "split-rupee",
-  storageBucket: "split-rupee.firebasestorage.app",
-  messagingSenderId: "802313510158",
-  appId: "1:802313510158:web:6a5f26fa88b388a3240b5f"
-};
